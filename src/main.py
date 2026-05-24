64f5e1b864760b24b1a130d32a3539cac2c85420
Kivanc Gordu <kivancgordu@hotmail.com>
2026-05-24 22:06:22 +0200

    feat: add resource monitoring

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import asyncio
 import httpx
 import json
+import psutil
+import subprocess
 from time import perf_counter_ns
 
 URL= "http://localhost:11434/api/generate"
-
-#MODEL = "llama3.1:8b"
-#MODEL2 = "gemma3:4b"
+HOST_ID = "localhost"
+MONITORING_SCOPE = "ollama_host_local"
+LLM_MODEL = "llama3.1:8b"
 
 LONG_CONTEXT = """
 Linguistic relativity asserts that language influences worldview or cognition. One form of linguistic relativity, linguistic determinism, regards peoples' languages as determining and influencing the scope of cultural perceptions of their surrounding world.
@@ -60,23 +62,82 @@ WORKLOADS = {
     },
 }
 
-async def run_one_user(client, user_id, model, workload):
+
+def collect_gpu_resources():
+    try:
+        out = subprocess.check_output([
+            "nvidia-smi",
+            "--query-gpu=utilization.gpu,memory.used,memory.total,temperature.gpu",
+            "--format=csv,noheader,nounits"
+        ], text=True)
+
+        host_gpu_util_percent, host_vram_used, host_vram_total_mb, host_gpu_temperature = map(
+            int,
+            out.strip().split(", ")
+        )
+    except:
+        host_gpu_util_percent = None
+        host_vram_used = None
+        host_vram_total_mb = None
+        host_gpu_temperature = None
+
+    return host_gpu_util_percent, host_vram_used, host_vram_total_mb, host_gpu_temperature
+     
+
+def collect_resources():
+    sample_timestamp_ns = perf_counter_ns()
+
+    host_cpu_percent = psutil.cpu_percent()
+
+    memory = psutil.virtual_memory()
+    host_memory_used = memory.used / 1024**2
+    host_memory_percent = memory.percent
+
+    disk_io = psutil.disk_io_counters()
+    host_disk_read_mb_total = disk_io.read_bytes / 1024**2
+    host_disk_write_mb_total = disk_io.write_bytes / 1024**2
+
+    host_gpu_util_percent, host_vram_used, host_vram_total_mb, host_gpu_temperature = collect_gpu_resources()
+
+    resources_sample = {
+        "host_id": HOST_ID,
+        "monitoring_scope": MONITORING_SCOPE,
+        "sample_timestamp_ns": sample_timestamp_ns,
+        "host_cpu_percent": host_cpu_percent,
+        "host_memory_used": host_memory_used,
+        "host_memory_percent": host_memory_percent,
+        "host_disk_read_mb_total": host_disk_read_mb_total,
+        "host_disk_write_mb_total": host_disk_write_mb_total,
+        "host_gpu_util_percent": host_gpu_util_percent,
+        "host_vram_used": host_vram_used,
+        "host_vram_total_mb": host_vram_total_mb,
+        "host_gpu_temperature": host_gpu_temperature,
+    }
+    return resources_sample
+
+async def monitor_resources(stop_event, resource_samples):
+    while not stop_event.is_set():
+        resource_samples.append(collect_resources())
+        await asyncio.sleep(1)
+
+async def run_one_user(client, user_id, llm_model, workload):
     print(f"==> REQUEST: {user_id}")
 
-    result = await chat(client, model, workload)
+    result = await chat(client, user_id, llm_model, workload)
 
-    print(f"\nRESULT: {user_id}")
+    print()
+    print(f"<== RESULT: {user_id}")
     print(result)
     print("==========================")
     return result
 
-async def chat(client, model, workload):
+async def chat(client, user_id, llm_model, workload):
 
     prompt = workload["prompt"]
     num_predict = workload["num_predict"]
 
     payload = {
-        "model": model,
+        "model": llm_model,
         "prompt": prompt,
         "stream": True,
         "options": {
@@ -87,12 +148,12 @@ async def chat(client, model, workload):
     request_start_ns = perf_counter_ns()
 
     success = False
-    ttft_ns = None
-    total_latency_ns = None
+    client_ttft_ns = None
+    client_total_latency_ns = None
     generation_start_ns = None
     generation_time_ns = None
     output_token_count = 0
-    tps = None
+    client_tps = None
 
 
     async with client.stream("POST", URL, json=payload) as response:
@@ -109,32 +170,33 @@ async def chat(client, model, workload):
 
             token = chunk.get("response", "")
             if token:
-                if ttft_ns is None:
+                if client_ttft_ns is None:
                     now_ns = perf_counter_ns()
-                    ttft_ns = now_ns - request_start_ns
+                    client_ttft_ns = now_ns - request_start_ns
                     generation_start_ns = now_ns
                 #print(token, end="", flush=True)
                 # for debugging, !can be mixed with concurrency
 
             if chunk.get("done") is True:
                 done_ns = perf_counter_ns()
-                total_latency_ns = done_ns - request_start_ns
+                client_total_latency_ns = done_ns - request_start_ns
                 output_token_count = chunk.get("eval_count", 0)
 
                 if generation_start_ns is not None:
                     generation_time_ns = done_ns - generation_start_ns
 
                     if output_token_count:
-                        tps = output_token_count / (generation_time_ns / 1_000_000_000)
+                        client_tps = output_token_count / (generation_time_ns / 1_000_000_000)
                 success = True
                 break
 
     return {
-        "model": model,
+        "user_id": user_id,
+        "llm_model": llm_model,
         "success": success,
-        "ttft_ns": ttft_ns,
-        "total_latency_ns": total_latency_ns,
-        "tps": tps,
+        "client_ttft_ns": client_ttft_ns,
+        "client_total_latency_ns": client_total_latency_ns,
+        "client_tps": client_tps,
         "workload_name": workload["name"],
         "num_predict": workload["num_predict"],
         "prompt_category": workload["prompt_category"],
@@ -142,19 +204,27 @@ async def chat(client, model, workload):
     }
 
 async def main():
-    model = "llama3.1:8b"
+    llm_model = LLM_MODEL
     user_amount = 3
     results = []
-    workload = WORKLOADS["long_prompt_long_output"]
-
+    resource_samples = []
+    workload = WORKLOADS["short_prompt_short_output"]
     benchmark_start_ns = perf_counter_ns()
+    stop_event = asyncio.Event()
 
     async with httpx.AsyncClient(timeout=None) as client:
+        monitor_task = asyncio.create_task(
+            monitor_resources(stop_event, resource_samples)
+        )
+
         tasks = [
-            run_one_user(client, i+1, model, workload)
+            run_one_user(client, i+1, llm_model, workload)
             for i in range(user_amount)
         ]
         results = await asyncio.gather(*tasks)
+
+        stop_event.set()
+        await monitor_task
 
     request_count = len(results)
     success_count = sum(1 for result in results if result["success"])
@@ -165,9 +235,13 @@ async def main():
 
     print()
     print(f"=== SIMULATION IS DONE ===")
-    print(f"=== SUCCESS RATE: {success_rate * 100:.0f}%")
-    print(f"=== RPS: {rps:.2f}")
-    print(f"=== DURATION: {total_time_s:.2f}s")
+    print(f"SUCCESS RATE: {success_rate * 100:.0f}%")
+    print(f"RPS: {rps:.2f}")
+    print(f"DURATION: {total_time_s:.2f}s")
+
+    print(f"RESOURCE SAMPLES: {len(resource_samples)}")
+    print(f"FIRST SAMPLE: {resource_samples[0]}")
+    print(f"LAST SAMPLE: {resource_samples[-1]}")
 
 if __name__ == "__main__":
     asyncio.run(main())
